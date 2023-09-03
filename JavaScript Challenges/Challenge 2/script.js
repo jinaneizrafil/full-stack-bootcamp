bfa6a3d9fade520dcc505728be1780783fcdcd1c
jinaneizrafil <izrafiljinane@gmail.com>
2023-09-02 20:56:45 -0800

    Challenge 2 - Done with step 3

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -37,3 +37,19 @@ if (age >= 22 && hasDriverLicense && !hasCar) {
 if (age < 18 && !hasDriverLicense && !hasCar) {
   needDriver = true;
 }
+// Step 3
+let role = "admin";
+
+switch (role) {
+  case "admin":
+    console.log("You have read and write access for the data.");
+    break;
+  case "user":
+    console.log("You have only read access for the data.");
+    break;
+  case "guest":
+    console.log("You have no access for the data.");
+    break;
+  default:
+    console.log("User role is not recognized.");
+}
