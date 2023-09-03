b4e604de19587b9049bb92effd53700aaf38d763
jinaneizrafil <izrafiljinane@gmail.com>
2023-09-02 18:45:47 -0800

    challenge1 step 2 and 3 done

---
+++ JavaScript Challenges/Challenge 1/script1.js
@@ -0,0 +1,14 @@
+let name = "Jinane";
+alert(name);
+let age = 25;
+const lastName = "Izrafil";
+var DOB = "12/05/1998";
+
+//changing values
+age = 33;
+DOB = "12/5/1990";
+
+// Log all three variables
+console.log(age);
+console.log(lastName);
+console.log(DOB);
