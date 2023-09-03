7de1afc5cd32c965e43392d50ec4992e0e5d437c
jinaneizrafil <izrafiljinane@gmail.com>
2023-09-02 18:52:20 -0800

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script1.js
+++ JavaScript Challenges/Challenge 1/script1.js
@@ -12,3 +12,30 @@ DOB = "12/5/1990";
 console.log(age);
 console.log(lastName);
 console.log(DOB);
+
+//step4
+console.log(typeof "This is a text");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1215);
+console.log(typeof "999");
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof undefined);
+
+let num = 42;
+let str = "Hello";
+let bool = true;
+let obj = { key: "value" };
+let arr = [1, 2, 3];
+let n = null;
+let u = undefined;
+
+console.log(typeof num, num);
+console.log(typeof str, str);
+console.log(typeof bool, bool);
+console.log(typeof obj, obj);
+console.log(typeof arr, arr);
+console.log(typeof n, n);
+console.log(typeof u, u);
