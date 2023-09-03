e883160ebb0dbd1801b7e898753f0e0991f615de
jinaneizrafil <izrafiljinane@gmail.com>
2023-09-02 21:05:28 -0800

    Challenge 3 - Done with step 1

---
+++ JavaScript Challenges/Challenge 3/script3.js
@@ -0,0 +1,45 @@
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+console.log(addNumbers(5, 3)); // 8
+
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    return "Both are equal";
+  }
+}
+
+function compute(num1, num2, operation) {
+  function add() {
+    return num1 + num2;
+  }
+
+  function subtract() {
+    return num1 - num2;
+  }
+
+  function multiply() {
+    return num1 * num2;
+  }
+
+  function divide() {
+    return num1 / num2;
+  }
+
+  switch (operation) {
+    case "add":
+      return add();
+    case "subtract":
+      return subtract();
+    case "multiply":
+      return multiply();
+    case "divide":
+      return divide();
+    default:
+      return "Invalid operation";
+  }
+}
